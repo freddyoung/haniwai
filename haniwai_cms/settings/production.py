265efeb7b4e39fa96da7fc660643395a35e03f18
Freddy Young <fmyoung96@gmail.com>
2025-03-21 22:19:28 +1000

    Changes to whitenoise

--- haniwai_cms/settings/production.py
+++ haniwai_cms/settings/production.py
@@ -5,6 +5,8 @@ SECRET_KEY = config("SECRET_KEY", default="dev-fallback-key")
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="*", cast=lambda v: v.split(','))
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 try:
     from .local import *
