e9946a59143f024f8bbacaf467dd0c5ebcc66c6a
Freddy Young <fmyoung96@gmail.com>
2025-06-09 22:24:24 +1000

    more changes

--- haniwai_cms/urls.py
+++ haniwai_cms/urls.py
@@ -7,12 +7,17 @@ from wagtail import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 from search import views as search_views
 
-from django.contrib.sitemaps.views import sitemap
+from django.contrib.sitemaps.views import sitemap as django_sitemap_view
+from django.http import HttpResponse
 from home.sitemaps import FinalHaniwaiSitemap
 
-sitemaps = {
-    'wagtail' : FinalHaniwaiSitemap,
-}
+# Custom sitemap view to remove the X-Robots-Tag header
+def sitemap_view(request):
+    response = django_sitemap_view(request, sitemaps={"wagtail": FinalHaniwaiSitemap})
+    response["Content-Type"] = "application/xml"
+    if "X-Robots-Tag" in response:
+        del response["X-Robots-Tag"]
+    return response
 
 urlpatterns = [
     path("django-admin/", admin.site.urls),
@@ -20,24 +25,16 @@ urlpatterns = [
     path("documents/", include(wagtaildocs_urls)),
     path("search/", search_views.search, name="search"),
     path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
-    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="sitemap"),
-    
-    ]
+    path("sitemap.xml", sitemap_view, name="sitemap"),
+]
 
 if settings.DEBUG:
     from django.conf.urls.static import static
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-    # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns = urlpatterns + [
-    # For anything not caught by a more specific rule above, hand over to
-    # Wagtail's page serving mechanism. This should be the last pattern in
-    # the list:
+urlpatterns += [
     path("", include(wagtail_urls)),
-    # Alternatively, if you want Wagtail pages to be served from a subpath
-    # of your site, rather than the site root:
-    #    path("pages/", include(wagtail_urls)),
 ]
